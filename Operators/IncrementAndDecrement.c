4dd107848974c8e51b1e80b9a211b550bb1fab52
Adeilsoara <adeilsonaragao@hotmail.com>
2022-04-04 14:08:35 -0300

    Increment and Decrements

---
+++ Operators/IncrementAndDecrement.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+
+    int a, b;
+
+    a = 0;
+    b = a++;
+    printf("After postincrement:\n");
+    printf("a: %d\n", a);
+    printf("b: %d\n", b);
+
+    a = 0;
+    b = ++a;
+    printf("After preincrement:\n");
+    printf("a: %d\n", a);
+    printf("b: %d\n", b);
+
+    return EXIT_SUCCESS;
+}
