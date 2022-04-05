80a9495f6fcdd0e629a3cef83973cc7f63a1866b
Adeilsoara <adeilsonaragao@hotmail.com>
2022-04-04 21:54:21 -0300

    Logical Operators

---
+++ Operators/LogicalOperators.c
@@ -0,0 +1,29 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<stdbool.h>
+
+int main(){
+    printf("Truth Table\n");
+    printf("-----------------------------------\n");
+    printf("| x | y |x AND y | x OR y | NOT x |\n");
+    printf("-----------------------------------\n");
+
+    bool x, y;
+
+    x = 0;
+    y = 0;
+    printf("| %d | %d |%5d   |%5d   |%4d   |\n", x, y, x && y, x || y, !x);
+    
+    x = 1;
+    y = 0;
+    printf("| %d | %d |%5d   |%5d   |%4d   |\n", x, y, x && y, x || y, !x);
+
+    x = 0;
+    y = 1;
+    printf("| %d | %d |%5d   |%5d   |%4d   |\n", x, y, x && y, x || y, !x);
+
+    x = 1;
+    y = 1;
+    printf("| %d | %d |%5d   |%5d   |%4d   |\n", x, y, x && y, x || y, !x);
+    return EXIT_SUCCESS;
+}
