0083d31cc6404c015ae0ac225ee4e31f8bd2356f
Adeilsoara <adeilsonaragao@hotmail.com>
2022-04-05 22:08:51 -0300

    Exercice - 02

---
+++ Exercices/List - Introduction/question02.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("----------------------------\n");
+    printf("------ TABUADA EM C --------\n");
+    printf("----------------------------\n");
+    printf(" 1 + 1 = 2\n");
+    printf(" 1 + 2 = 3\n");
+    printf(" 1 + 3 = 4\n");
+    printf(" 1 + 4 = 5\n");
+    printf(" 1 + 6 = 7\n");
+    printf(" 1 + 7 = 8\n");
+    printf(" 1 + 8 = 9\n");
+    printf(" 1 + 9 = 10");
+    return EXIT_SUCCESS;
+}
