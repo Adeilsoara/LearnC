2daf602bab9d3cd6fd7a46b83fb3e5ee64d7acaa
Adeilsoara <adeilsonaragao@hotmail.com>
2022-04-08 10:46:55 -0300

    Loop For

---
+++ ControlFlow/forLoop.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/* For Loop  */
+/* int main (){
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("i: %d\n", i);
+    }
+
+    return EXIT_SUCCESS;
+} */
+
+/* Inverse For Loop */
+
+/* int main(){
+    for (int i = 5; i >= 0; i--)
+    {
+        printf("i = %d\n", i);
+    }
+
+    return EXIT_SUCCESS;
+} */
+
+/* Converting For Loop in While Loop */
+
+int main(){
+    int i = 5;
+    while (i >= 0)
+    {
+        printf("i = %d\n", i);
+        i--;
+    }
+    
+    return EXIT_SUCCESS;
+}
