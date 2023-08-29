e964f209083f3992a3484559f988baf357b9b2c0
Adeilsoara <adeilsonaragao@hotmail.com>
2023-08-28 22:04:11 -0300

    Implements ternario operator

--- Exercices_Intro_C/question10.c
+++ Exercices_Intro_C/question10.c
@@ -11,23 +11,26 @@ int main()
     int num1, num2;
     scanf("%d", &num1);
     scanf("%d", &num2);
-    if (num1 == num2)
+/*     if (num1 == num2)
     {
         printf("O %d e o %d sao iguais", num1, num2);
     }
     else
     {
         printf("O %d e o %d sao diferentes \n", num1, num2);
-    }
+    } */
 
-    if (num1 > num2)
+    num1 == num2 ? printf("Iguais \n") : printf("Diferentes \n");
+    
+   /*  if (num1 > num2)
     {
         printf("%d eh maior que o %d", num1, num2);
     }
     else
     {
         printf("%d eh menor que o %d", num1, num2);
-    }
+    } */
     
+    num1 > num2 ? printf("%d Maior que %d", num1, num2) : printf(" %d Menor que %d", num1,num2);
     
 }
